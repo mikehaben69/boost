85f3be4ec60508de03f27be5925aef0f2d6eb528
Mike Haben <michael.haben@btinternet.com>
2021-10-04 20:46:17 +0100

    Corrected indentation in rep_timer_demo.cpp

--- asio/demo_rep_timer/rep_timer_demo.cpp
+++ asio/demo_rep_timer/rep_timer_demo.cpp
@@ -35,33 +35,33 @@ void OnTimer3(boost::system::error_code const& error)
 
 int main(void)
 {
-    boost::asio::io_context io_ctx;
+	boost::asio::io_context io_ctx;
 
-    boost::asio::deadline_timer test_timer_1(make_strand(io_ctx));
-    boost::asio::any_io_executor ex = test_timer_1.get_executor();
-    boost::asio::any_io_executor ex2 = make_strand(io_ctx);
+	boost::asio::deadline_timer test_timer_1(make_strand(io_ctx));
+	boost::asio::any_io_executor ex = test_timer_1.get_executor();
+	boost::asio::any_io_executor ex2 = make_strand(io_ctx);
 
-    boost::asio::deadline_timer test_timer_2(ex);
-    boost::asio::deadline_timer test_timer_3(ex2);
+	boost::asio::deadline_timer test_timer_2(ex);
+	boost::asio::deadline_timer test_timer_3(ex2);
 
-    boost::asio::any_io_executor ex3 = make_strand(io_ctx);
-    boost::asio::repeating_timer rep_timer_1(ex3);
+	boost::asio::any_io_executor ex3 = make_strand(io_ctx);
+	boost::asio::repeating_timer rep_timer_1(ex3);
 
 	std::cout << "Starting some timers..." << std::endl;
 
 	test_timer_1.expires_from_now( boost::posix_time::milliseconds(2900) );
-    test_timer_1.async_wait(OnTimer1);
+	test_timer_1.async_wait(OnTimer1);
 	test_timer_2.expires_from_now( boost::posix_time::milliseconds(900) );
-    test_timer_2.async_wait(OnTimer2);
+	test_timer_2.async_wait(OnTimer2);
 	test_timer_3.expires_from_now( boost::posix_time::milliseconds(1900) );
-    test_timer_3.async_wait(OnTimer3);
+	test_timer_3.async_wait(OnTimer3);
 
-    rep_timer_1.start( boost::posix_time::milliseconds(1000), OnRepTimer );
+	rep_timer_1.start( boost::posix_time::milliseconds(1000), OnRepTimer );
 
 	boost::asio::io_service::work work(io_ctx);  // force io_ctx to keep running until explicitly stopped
-    io_ctx.run();
+	io_ctx.run();
 
 	std::cout << "Timers are running" << std::endl;
-    for(;;) {}
+	for(;;) {}
 }
 
